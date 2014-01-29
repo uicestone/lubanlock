fe035eccf68e4c5f65d78abfa81587d30a790b88
陆秋石 <uicestone@gmail.com>
2014-01-30 00:47:22 +0800

    将登录错误抛出工作交给用户模型

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -15,15 +15,9 @@ class user extends SS_controller{
 			
 			$user = $this->user->verify($this->input->get_post('username'), $this->input->get_post('password'));
 
-			if($user){
-				$this->session->set_userdata('user_id', intval($user['id']));
-				$this->user->updateLoginTime();
-				redirect();
-			}
-			
-			else{
-				throw new Exception('login_info_error', 403);
-			}
+			$this->session->set_userdata('user_id', intval($user['id']));
+			$this->user->updateLoginTime();
+			redirect();
 		}
 		
 		$this->load->view('login', compact('alert'));
@@ -47,78 +41,5 @@ class user extends SS_controller{
 		$this->load->view('user/profile_sidebar',true,'sidebar');
 	}
 	
-	function submit($submit){
-		
-		$this->load->library('form_validation');
-		
-		try{
-			
-			if($submit=='profile'){
-				if($this->input->post('password_new')){
-					if($this->user->verify($this->user->name, $this->input->post('password'))===false){
-						throw new Exception('当前密码错误');
-					}
-					if($this->input->post('password_new')!==$this->input->post('password_new_confirm')){
-						throw new Exception('两次密码输入不一致');
-					}
-					$this->user->updatePassword($this->user->id, $this->input->post('password_new'));
-					$this->output->message('用户名/密码修改成功');
-				}
-				
-				if($this->input->post('username') && $this->input->post('username')!==$this->user->name){
-					$this->form_validation->set_rules('username','用户名','is_unique[user.name]');
-					if($this->form_validation->run()!==false){
-						$this->user->updateUsername($this->user->id, $this->input->post('username'));
-					}else{
-						throw new Exception(validation_errors());
-					}
-				}
-				
-				$people=$this->input->sessionPost('people');
-				$meta=$this->input->sessionPost('people_meta');
-				
-				$this->people->update($this->user->id,$people);
-				$this->people->updateMetas($this->user->id, $meta);
-				
-				$this->output->message($this->output->title.' 已保存');
-			}
-
-			elseif($submit=='signup'){
-
-				$this->form_validation->set_rules('username','用户名','required|is_unique[user.name]')
-						->set_rules('password','密码','required')
-						->set_rules('password_confirm','确认密码','matches[password]')
-						->set_message('matches','两次密码输入不一致');
-
-				if($this->form_validation->run()===false){
-					$this->output->message(validation_errors(),'warning');
-					throw new Exception;
-				}
-				
-				$data=array(
-					'name'=>$this->input->post('username'),
-					'password'=>$this->input->post('password')
-				);
-				
-				$user_id=$this->user->add($data);
-				
-				$this->session->set_userdata('user/id',$user_id);
-				$this->user->__construct($user_id);
-				
-				$this->output->status='redirect_href';
-				$this->output->data='';
-			}
-		}
-		catch (Exception $e){
-			if($e->getMessage()){
-				$this->output->message($e->getMessage(), 'warning');
-			}
-			$this->output->status='failed';
-		}
-		
-		if(is_null($this->output->status)){
-			$this->output->status='success';
-		}
-	}
 }
 ?>
