3ca31203ef2eb27e5137eb23fe594847c4159c20
陆秋石 <uicestone@gmail.com>
2014-03-14 20:59:40 +0800

    这是一个后端组件，剥除前端部分

--- application/views/login.php
+++
@@ -1,5 +0,0 @@
-<form>
-	用户名：<input type="text" name="username">
-	密码：<input type="password" name="password">
-	<button type="submit">登录</button>
-</form>
--- web/filters.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* Filters */
-
-angular.module('lubanlockFilters', []).filter('checkmark', function() {
-  return function(input) {
-    return input ? '\u2713' : '\u2718';
-  };
-});
