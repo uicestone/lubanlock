5c6e4fc60ed5815985fe163076e879b8f9263660
陆秋石 <uicestone@gmail.com>
2014-03-20 02:56:24 +0800

    index控制器移入Welcome

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,53 +1,36 @@
 <?php
-/**
- * CodeIgniter
- *
- * An open source application development framework for PHP 5.2.4 or newer
- *
- * NOTICE OF LICENSE
- *
- * Licensed under the Academic Free License version 3.0
- *
- * This source file is subject to the Academic Free License (AFL 3.0) that is
- * bundled with this package in the files license_afl.txt / license_afl.rst.
- * It is also available through the world wide web at this URL:
- * http://opensource.org/licenses/AFL-3.0
- * If you did not receive a copy of the license and are unable to obtain it
- * through the world wide web, please send an email to
- * licensing@ellislab.com so we can send you a copy immediately.
- *
- * @package		CodeIgniter
- * @author		EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
- * @license		http://opensource.org/licenses/AFL-3.0 Academic Free License (AFL 3.0)
- * @link		http://codeigniter.com
- * @since		Version 1.0
- * @filesource
- */
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Welcome extends LB_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
+class Welcome extends LB_Controller{
+	function __construct() {
+		parent::__construct();
+	}
+	
+	function index(){
+		
+		if(!$this->user->isLogged()){
+			redirect('login');
+		}
+		
+		$this->load->view('index');
+	}
+	
+	function robots(){
+		$this->output->set_output($this->config->user_item('robots'));
+	}
+	
+	function favicon(){
+		
+		$this->output->set_content_type('ico');
+		
+		foreach(array(
+			APPPATH.'../web/images/favicon_'.$this->company->syscode.'.ico',
+			APPPATH.'../web/images/favicon_'.$this->company->type.'.ico',
+			APPPATH.'../web/images/favicon.ico',
+		) as $path){
+			if(file_exists($path)){
+				readfile($path);
+			}
+		}
 	}
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/Welcome.php */
+?>
