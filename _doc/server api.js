8cc5515fcbd6c6af7361d6a870c3e1b9c084e590
陆秋石 <uicestone@gmail.com>
2014-03-26 02:19:58 +0800

    Server API 更新

--- _doc/server api.js
+++ _doc/server api.js
@@ -1,4 +1,67 @@
 /**
+ * data structure definition
+ * /
+
+/**
+ *  对象数据结构
+ */
+var object = {
+	"id":0,
+	"type":"",//类型，如"人员", "联系人", "案件", "事务"
+	"num":"",//编号，字符串
+	"name":"",//显示名称
+	"meta":meta,
+	"relative":relative,
+	"status":status,
+	"tag":tag
+};
+
+/*
+ * 元数据
+ * 存放对象的普通属性，如人员的电话，案件的简介，考试的参与人数、班级的教室等
+ */
+var meta = {
+	"{key}": ["{value1}", "{value2}"]
+};
+
+/**
+ * 关联对象
+ * 描述与本对象关联的对象的简要信息，以及关系
+ */
+var relative = {
+	"{relation}": [
+		{
+			"id": 0,
+			"type":"",//关联对象的object.type
+			"name": "",//关联对象的object.name
+			"num":"",//关联对象的object.num
+			"relationship_num":"",//关系的编号
+			meta:{//关系的元数据
+				"{key}":"{value}"
+			}
+		}
+	]
+};
+
+/*
+ * 一个对象的各种与时间有关的状态
+ * 如，案件的立案时间，帐目的应收帐款时间，日程的底线时间
+ */
+var status = [
+	{
+		"name":"",
+		"date":"1970-01-01 00:00:00"
+	}
+];
+
+/*
+ * 一个对象的标签，用于搜索和分类
+ */
+var tag = {
+	"{taxonomy}": ["{tag1}","{tag2}"]
+};
+
+/**
  * server api
  */
 var api = [
@@ -7,11 +70,10 @@ var api = [
 		"name":"获取单个对象",
 		"request":{
 			"method":"GET",
-			"path":"/object/{id}",
-			/*query指url中的query string，也可以用?query={json}来传递*/
+			"path":"/object/" + object.id,
 			"query":{
-				"with_meta":true,
-				"with_relative":true,
+				"with_meta":true | getMetaArgs,
+				"with_relative":true | getStatusArgs,
 				"with_status":true,
 				"with_tag":true,
 				"with":["meta","relative","status","tag"]
@@ -19,19 +81,7 @@ var api = [
 		},
 		"response":{
 			"contentType":"application/json",/*contentType默认都为application/json*/
-			"body":objectData
-		}
-	},
-	{
-		"name":"更新单个对象",
-		"request":{
-			"method":"POST",
-			"path":"/object/{id}",
-			"contentType":"application/json",/*PUT和POST的contentType默认都为application/json*/
-			"body":objectData
-		},
-		response:{
-			"body":objectData
+			"body":object
 		}
 	},
 	{
@@ -39,10 +89,21 @@ var api = [
 		"request":{
 			"method":"PUT/POST",
 			"path":"/object",
-			"body":objectData//without id, uid, time, timeinsert attributes
+			"body":object//without id, uid, time, timeinsert attributes
 		},
 		response:{
-			"body":objectData
+			"body":object
+		}
+	},
+	{
+		"name":"更新单个对象",
+		"request":{
+			"method":"PUT",
+			"path":"/object/" + object.id,
+			"body":object
+		},
+		response:{
+			"body":object
 		}
 	},
 	{
@@ -52,12 +113,12 @@ var api = [
 			"query":listArgs
 		},
 		"response":{
-			"body":{
-				"total":1000,//去除分页参数后的对象数目
-				"data":[
-					objectData
-				]
-			}
+			"headers":{
+				"Total-Objects":1000,//去除分页参数后的对象数目
+			},
+			"body":[
+				object
+			]
 		}
 	},
 	
@@ -71,27 +132,30 @@ var api = [
 	{
 		"name":"为一个对象添加一个元数据",
 		"request":{
-			"method":"PUT/POST",
-			"path":"/object/{id}/meta",
-			"body":meta//without "id" attribute
+			"method":"POST",
+			"path":"/object/" + object.id + "/meta/" + meta.key,
+			"query":{},
+			"body":value//without "id" attribute
 		},
 		"response":{
-			"body":meta
+			"body":meta//all meta key-values of the object
 		}
 	},
 	{
 		"name":"更新对象的一个元数据",
 		"request":{
-			"method":"POST",
-			"path":"/object/{id}/meta",
-			"body":meta
+			"method":"PUT",
+			"path":"/object/" + object.id + "/meta/" + meta.key,
+			"query":{},
+			"body":value
 		}
 	},
 	{
 		"name":"删除一个对象的一个元数据",
 		"request":{
 			"method":" DELETE",
-			"path":"/object/{id}/meta",
+			"path":"/object/" + object.id + "/meta/" + meta.key,
+			"query":{},
 			"query":meta//with "id" or some attributes for resource locating
 		}
 	},
@@ -116,8 +180,9 @@ var api = [
 	{
 		"name":"为一个对象添加一个状态",
 		"request":{
-			"method":"PUT/POST",
-			"path":"/object/{id}/status",
+			"method":"POST",
+			"path":"/object/" + object.id + "/status/" + status.name,
+			"query":{},
 			"body":status//without "id" attribute
 		},
 		"response":{
@@ -127,8 +192,9 @@ var api = [
 	{
 		"name":"更新对象的一个状态",
 		"request":{
-			"method":"POST",
-			"path":"/object/{id}/status",
+			"method":"PUT",
+			"path":"/object/" + object.id + "/status/" + status.name,
+			"query":{},
 			"body":status
 		}
 	},
@@ -136,8 +202,8 @@ var api = [
 		"name":"删除一个对象的一个状态",
 		"request":{
 			"method":" DELETE",
-			"path":"/object/{id}/status",
-			"query":status//with "id" or some attributes for resource locating
+			"path":"/object/" + object.id + "/status/" + status.name,
+			"query":{},
 		}
 	},
 	{
@@ -159,29 +225,22 @@ var api = [
 	 * relative
 	 */
 	{
-		"name":"为一个对象添加一个相关对象",
+		"name":"为一个对象添加或更新一个相关对象",
 		"request":{
-			"method":"PUT/POST",
-			"path":"/object/{id}/relative",
-			"body":relative//without "id","name" and "type" attribute
+			"method":"POST",
+			"path":"/object/" + object.id + "/relative/" + relative.role,
+			"query":{},
+			"body":object.id//without "id","name" and "type" attribute
 		},
 		"response":relative
 
 	},
 	{
-		"name":"更新对象的一个相关对象",
-		"request":{
-			"method":"POST",
-			"path":"/object/{id}/relative",
-			"body":relative//without "name" and "type" attribute
-		}
-	},
-	{
 		"name":"删除一个对象的一个相关对象",
 		"request":{
 			"method":" DELETE",
-			"path":"/object/{id}/relative",
-			"query":relative//with "id" or some attributes for resource locating
+			"path":"/object/" + object.id + "/relative/" + relative.role,
+			"query":{}
 		}
 	},
 	{
@@ -200,63 +259,21 @@ var api = [
 			"主办律师","协办律师","案源人"
 		]
 	},
-	{
-		"name":"为一个对象和另一个对象的关系添加一个状态(设为肯定)",
-		"request":{
-			"method":"PUT/POST",
-			"path":"/object/{id}/relativemod",
-			"query":{
-				"id":0,//关系的id
-				"relative":0//关联对象的id
-			},
-			"body":{
-				name:"",//开关量的名称
-			}
-		}
-	},
-	{
-		"name":"删除一个对象和另一个对象的关系的状态(设为否定)",
-		"request":{
-			"method":" DELETE",
-			"path":"/object/{id}/relativemod",
-			"query":{
-				"id":0,//关系的id
-				"relative":0,//关联对象的id
-				name:"",//开关量的名称
-			}
-		}
-	},
 	
 	/*
 	 * tag
 	 */
 	{
-		"name":"为一个对象添加一个标签",
+		"name":"为一个对象设置标签",
 		"request":{
-			"method":"PUT/POST",
-			"path":"/object/{id}/tag",
-			"body":tag//with "name" or "id"
+			"method":"POST",
+			"path":"/object/" + object.id + "/tag/" + tag.taxonomy,
+			"body":"tag" | ["tags"]
 		},
 		"response":{
 			"body":tag
 		}
 
-	},
-	{
-		"name":"更新对象的一个标签",
-		"request":{
-			"method":"POST",
-			"path":"/object/{id}/tag",
-			"body":tag
-		}
-	},
-	{
-		"name":"删除一个对象的一个标签",
-		"request":{
-			"method":" DELETE",
-			"path":"/object/{id}/tag",
-			"query":tag//with "id" or some attributes for resource locating
-		}
 	},
 	{
 		"name":"推荐的tag.name",
@@ -271,6 +288,13 @@ var api = [
 		"response":[
 			"潜在客户","成交客户"
 		]
+	},
+	
+	/**
+	 * permission
+	 */
+	{
+		"name":"为一个对象设置权限"
 	},
 	
 	
@@ -377,152 +401,56 @@ var api = [
 ];
 
 /**
- * 对象数据结构
- */
-var objectData={
-	"id":0,
-	"type":"",//对象类型如"人员", "联系人", "案件", "事务"
-	"num":"",//对象的编号，字符串
-	"name":"",//对象的显示名称
-	"meta":{//非必有，获得对象时with_meta参数决定
-		"<metaName>": ["metaValue1", "metaValue2"]
-	},
-	"relative":{//非必有，获得对象时with_relative参数决定
-		"<relation>": [
-			{
-				"id": 0,
-				"type":"",
-				"name": ""
-			},{
-				"id": 0,
-				"type":"",
-				"name": ""
-			}
-		]
-	},
-	"status":[//非必有，获得对象时with_status参数决定
-		{
-			"name":"",
-			"date":"1970-01-01 00:00:00"
-		},
-		{
-			"name":"",
-			"date":"1970-01-01 00:00:00"
-		},
-	],
-	"tag":{//非必有，获得对象时with_tag参数决定
-		"taxonomy": "tag",
-		"领域": "刑事",
-		"阶段": "二审"
-	}
-};
-
-/*
- * 元数据
- * 存放对象的普通属性，如人员的电话，案件的简介，考试的参与人数、班级的教室等
- */
-var meta={
-	"id":0,
-	"key":"",
-	"value":"",
-	"comment":""
-};
-
-/**
- * 关连对象
- * 描述与本对象关联的对象的简要信息，以及关系
- */
-var relative={
-	"id":0,//关系id
-	"num":"",//关系编号，比如一个学生在一个班级中的学号
-	"relation":"",
-	"mod":mod,//一组可变权限/状态名
-	"weight":0.00,//比重，同relation的比重之和不应超过1
-	"relative":0,//关联对象id
-	"type":"",//关连对象类型
-	"name":""//关连对象显示名称
-};
-
-/*
- * 一个对象的各种与时间有关的状态
- * 如，案件的立案时间，帐目的应收帐款时间，日程的底线时间
- */
-var status={
-	"id":0,
-	"name":"",
-	"type":"",
-	"datetime":"",
-	"content":"",
-	"comment":""
-};
-
-/*
- * 一个对象的标签，用于搜索和分类
- */
-var tag={
-	"id":0,//这是tag的id，而不是object-tag的关系id
-	"name":"",
-	"type":"",//标签类型，分类的分类，如“阶段”，“领域”,
-	"color":"#000"
-};
-
-/**
  * 对象列表搜索参数
  */
-var listArgs={
-	"search":"",
-	"id_in":[],
-	"id_less_than":0,
-	"id_greater_than":0,
-	"name":"",//模糊匹配
-	"type":"",//对象类别
-	"num":"",//模糊匹配
-	"display":true,
-	//"company":0,
-	"uid":0,//对象的创建人
+var listArgs = "{value}"/*直接匹配一个值，默认匹配的键为object.id*/ | ["{value}"]/*作为{in:[]}处理*/ | {
+	"search":"",//智能搜索
+	"id":0,//根据id获得对象
+	"name":listArgs,//usage: {"name":"Jason"}, {"name":["Jason","Mike","Marry"]}, {"name":{"in":["Jason","Mike","Marry"]}}
+	"type":listArgs,
+	"num":listArgs,
+	"time":listArgs,
+	"user":listArgs,
+	"meta":{"{key}": listArgs} | [listArgs],//前一种按键-值对搜索，后一种按键名
+	"status":{"{name}": listArgs} | [listArgs],//前一种按状态-时间对搜索，后一种按状态名
+	"tag":{"{taxonomy}": listArgs} | [listArgs],//前一种按分类方式-分类搜索，后一种按分类
+	"is_relative_of":{"{role}": listArgs} | [listArgs],//前一种按关系-关联对象搜索，后一种按关联对象
+	"has_relative_like":{"{role}": listArgs} | [listArgs],//前一种按关系-关联对象搜索，后一种按关联对象
+	"and, or":listArgs,//逻辑运算，如{"or":{"name":{"in":["A", "B"]}, "name":{"nin":["C","D"]}}。
+	"gt, gte, lt, lte, ne":"{value}",//算数运算
+	"in, nin":["{value}"],//in 或 not in
 
-	"tags":{"tagType":tagName,"0":tagName},//包含一组标签
-	"without_tags":{"tagType":tagName,"0":tagName},//不包含一组标签
-	"with_tags":false,
+	"with_meta":false | getMetaArgs,
+	"with_relative":false | getRelativeArgs,
+	"with_status":false | getStatusArgs,
+	"with_tag":false | getTagArgs,
 
-	"meta":{"metaName":metaContent,"0":metaName},//包含一组资料项，一组资料项为某值
-	"with_meta":false,
-
-	"is_relative_of":0,//根据本对象获得相关对象
-		"is_relative_of__relation":"",//只查找具有某关系的相关人
-	"has_relative_like":0,//根据相关对象获得本对象
-		"has_relative_like__relation":"",
-	"is_secondary_relative_of":0,//右侧相关对象的右侧相关对象，“下属的下属”
-		"is_secondary_relative_of__media":"",//中间对象的type
-	"is_both_relative_with":0,//右侧相关对象的左侧相关对象，“具有共同上司的同事”
-		"is_both_relative_with__media":"",
-	"has_common_relative_with":0,//左侧相关对象的右侧相关对象，“具有共同下属的上司”
-		"has_common_relative_with__media":"",
-	"has_secondary_relative_like":0,//左侧相关对象的左侧相关对象，“上司的上司”
-		"has_secondary_relative_like__media":"",
-	"with_relative":false,
-
-	"status":{
-		"name":[//存在两种判断方式
-			{"from":"from_syntax","to":"to_syntax","format":"timestamp/date/datetime"},
-			false//bool，仅过滤出包含或不包含该状态的对象
-		],
-		//例子
-		"首次接洽":{"from":1300000000,"to":1300100000,"format":"timestamp"},
-		"立案":{"from":"2013-01-01","to":"2013-06-30"},
-		"结案":true
-	},
-	"with_status":false,
-
-	"orderby":[//支持2种数据格式
-		"id desc, name asc",
+	"order_by":[//支持2种格式
+		"id desc",
 		[
 			["id","desc"],
 			["name","asc"]
 		]
 	],
-	"limit":[//支持2种数据格式
+	
+	//根据页码和每页行数分页
+	"page":1,
+	"per_page":25,
+	
+	//根据需要的行数和偏移行数分页
+	"limit":[//支持2种格式
 		10,//直接给出需要的行数,
 		[10,20]//需要的行数, 起点偏移
 	]
 };
+
+var getMetaArgs = {
+	as_rows: false
+}
+
+var getStatusArgs = {
+	as_rows: false,
+	id_only: false,
+	include_disabled: false,
+	with_meta: true
+}
