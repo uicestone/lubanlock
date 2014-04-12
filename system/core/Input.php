4758217b3e6abd3197c5f95274c9b26bf912efa9
Uice Lu <uicestone@gmail.com>
2014-04-13 00:55:34 +0800

    allow Chinese $_POST key

--- system/core/Input.php
+++ system/core/Input.php
@@ -744,7 +744,7 @@ class CI_Input {
 	 */
 	protected function _clean_input_keys($str, $fatal = TRUE)
 	{
-		if ( ! preg_match('/^[a-z0-9:_\/|-]+$/i', $str))
+		if ( ! preg_match('/^[a-z0-9:_\/|\-\x{4e00}-\x{9fa5}]+$/iu', $str))
 		{
 			if ($fatal === TRUE)
 			{
