3354a8c75e9ed1e92209f49fb1835b18df799611
Uice Lu <uicestone@gmail.com>
2014-04-17 14:21:10 +0800

    fix: 获得用户函数的一处问题

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -89,8 +89,18 @@ class User_model extends Object_model{
 	}
 
 	function fetch($id=null, array $args = array(), $permission_check = true){
+		
+		if(is_null($id)){
+			$id=$this->id;
+		}
+		elseif(!array_key_exists('set_id', $args) || $args['set_id']){
+			$this->id=$id;
+		}
+		
 		$object = parent::fetch($id, $args, $permission_check);
+		
 		$user = $this->db->select('user.id, user.name, user.email, user.roles, user.last_ip, user.last_login')->from('user')->where('id', $id)->get()->row_array();
+		
 		return array_merge($object, $user);
 	}
 	
