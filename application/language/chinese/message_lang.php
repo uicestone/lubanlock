d1003b824e1e0ab59cfd9c9f408c482b2054899b
Uice Lu <uicestone@gmail.com>
2014-05-24 23:53:23 +0800

    合并删除语言文件; 单元测试报告语言

--- application/language/chinese/message_lang.php
+++ application/language/chinese/message_lang.php
@@ -5,5 +5,6 @@ $lang['argument_error'] = '参数错误';
 $lang['no_permission_to_grant'] = '无权授权';
 $lang['login_info_error'] = '用户名或密码错误';
 $lang['object'] = '对象';
+$lang['user'] = '用户';
 $lang['not_found'] = '没有找到';
-$lang['duplicated_matches'] = '重复匹配';
+$lang['duplicated_matches']='无法确定唯一项';
--- application/language/chinese/unit_test_lang.php
+++ application/language/chinese/unit_test_lang.php
@@ -1,14 +1,14 @@
 <?php
 
-$lang['ut_test_name']		= 'Test Name';
-$lang['ut_test_datatype']	= 'Test Datatype';
-$lang['ut_res_datatype']	= 'Expected Datatype';
-$lang['ut_result']			= 'Result';
-$lang['ut_undefined']		= 'Undefined Test Name';
-$lang['ut_file']			= 'File Name';
-$lang['ut_line']			= 'Line Number';
-$lang['ut_passed']			= 'Passed';
-$lang['ut_failed']			= 'Failed';
+$lang['ut_test_name']		= '测试名称';
+$lang['ut_test_datatype']	= '数据类型';
+$lang['ut_res_datatype']	= '期望类型';
+$lang['ut_result']			= '结果';
+$lang['ut_undefined']		= '未定义测试名称';
+$lang['ut_file']			= '文件';
+$lang['ut_line']			= '行号';
+$lang['ut_passed']			= '通过';
+$lang['ut_failed']			= '失败';
 $lang['ut_boolean']			= 'Boolean';
 $lang['ut_integer']			= 'Integer';
 $lang['ut_float']			= 'Float';
