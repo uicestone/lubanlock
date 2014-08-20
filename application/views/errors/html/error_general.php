7e38ffe578c73ea753bb9a9ceaa8ff0575c62300
Uice Lu <uicestone@gmail.com>
2014-08-21 01:05:15 +0800

    美化错误页面

--- application/views/errors/html/error_general.php
+++ application/views/errors/html/error_general.php
@@ -26,65 +26,62 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Error</title>
-<style type="text/css">
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>错误</title>
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
-::-webkit-selection { background-color: #E13300; color: white; }
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		
+		<!--[if !IE]> -->
+		<script type="text/javascript" src="js/jquery/jquery.min.js?v=2.1.1"></script>
+		<!-- <![endif]-->
+		<!--[if IE]>
+		<script type="text/javascript" src="js/jquery/jquery-1.x.min.js?v=1.11.1"></script>
+		<![endif]-->
+					
+		<link rel="stylesheet" href="css/font-awesome.min.css?v=3.2.1" />
+		<link rel="stylesheet" href="css/bootstrap.min.css?v=3.2.0" />
+		<link rel="stylesheet" href="css/ace.css" />
+		
+		<!--[if lt IE 9]>
+		<link rel="stylesheet" href="css/ace-ie.css" />
+		<![endif]-->
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
+		<link rel="stylesheet" href="css/style.css?v=2014-08-18-1" />
+		
+		<!--[if lt IE 9]>
+		<script src="js/html5shiv.js"></script>
+		<script src="js/respond.min.js"></script>
+		<![endif]-->
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
+	</head>
 
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
+	<body>
+		<div class="main-container">
+			<div class="page-content">
+				<div class="row">
+					<div class="col-xs-12">
 
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
-</head>
-<body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
-</body>
+						<div class="error-container">
+							<div class="well">
+								<h1 class="grey lighter smaller">
+									<span class="blue bigger-125">
+										<i class="icon-sitemap"></i>
+										<?=$status_code?>
+									</span>
+									<?=$heading?>
+								</h1>
+								
+								<hr />
+								
+								<h3 class="lighter smaller"><?=$message?></h3>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
