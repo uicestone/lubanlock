634d50ebb38e2140787b7b2fd0d1aac5586ac407
Uice Lu <uicestone@gmail.com>
2014-09-14 11:24:04 +0800

    fix file download

--- application/controllers/File.php
+++ application/controllers/File.php
@@ -43,7 +43,7 @@ class File extends LB_Controller{
 	
 	function download($id){
 		
-		$file = new Object($id, array('with_meta'=>array('visibility'=>'>0')));
+		$file = (array) new Object($id, array('with_meta'=>array('visibility'=>'>0')));
 		
 		$path = get_meta($file, 'full_path');
 		
